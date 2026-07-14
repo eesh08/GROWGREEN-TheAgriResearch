7563a0bb383e56707269e1abb2d5c16bc6b1d7f2
Eesh <sr3659@srmist.edu.in>
2026-07-14 23:41:16 +0530

    Configure project for Railway deployment

--- VegetableShop/settings.py
+++ VegetableShop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -77,7 +78,7 @@ WSGI_APPLICATION = 'VegetableShop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-import dj_database_url
+
 
 DATABASES = {
     "default": dj_database_url.config(
